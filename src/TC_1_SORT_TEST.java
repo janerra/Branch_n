8b0b4f006edee55c4ed685c7568359c30b7a22f7
alex <al57kot@gmail.com>
2018-06-28 12:51:18 -0400

    New commit

--- src/TC_1_SORT_TEST.java
+++ src/TC_1_SORT_TEST.java
@@ -1,10 +1,9 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-
-import java.util.*;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
--- src/crossBrowsers/Parallel_Executing.java
+++ src/crossBrowsers/Parallel_Executing.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 
 
 //import TestConstants1;
-
+// some new staff
 
 
 public class Parallel_Executing {
